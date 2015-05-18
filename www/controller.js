d238910a5b5768d9e59eeb5b1e9d7fd4b230ae3f
raakeshkamal <raakeshkamal@gmail.com>
2015-05-18 16:13:35 +0530

    markers added

--- www/controller.js
+++ www/controller.js
@@ -25,64 +25,95 @@
     app.controller('MapController', function($scope, $timeout){
 
         var map;
+        var current;
         $scope.markers = [];
         $scope.markerId = 1;
         var start=new google.maps.LatLng(12.9915, 80.2336);
         var directionsDisplay;
-         var directionsService = new google.maps.DirectionsService();
+        var directionsService = new google.maps.DirectionsService();
         
 
         //Map initialization  
         $timeout(function(){
             var options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-};
-function error(err) {
-  console.warn('ERROR(' + err.code + '): ' + err.message);
-};
-            if(navigator.geolocation)
-                {navigator.geolocation.getCurrentPosition(
-                    function(position) {
-                        console.log(position);
-                     start = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-                                 var marker = new google.maps.Marker({
-      position: start,
-      map: map,
-      title: 'Hello World!'
-  });   
-                 },error,options)};
-             directionsDisplay = new google.maps.DirectionsRenderer();
-            var latlng = start;
-            var myOptions = {
-                zoom: 15,
-                center: latlng,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
+              enableHighAccuracy: true,
+              timeout: 5000,
+              maximumAge: 0
+          };
+          function error(err) {
+              console.warn('ERROR(' + err.code + '): ' + err.message);
+          };
+          if(navigator.geolocation)
+            {navigator.geolocation.getCurrentPosition(
+                function(position) {
+                    console.log(position);
+                    start = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+                    current = new google.maps.Marker({
+                      position: start,
+                      map: map,
+                      title: 'Hello World!',
+                       animation:google.maps.Animation.BOUNCE
+                  }); 
+                    google.maps.event.addListener(current, 'click', function() {                ons.notification.alert({
+                        message: 'You are here'
+                    });});  
+                },error,options)};
 
-            map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
-            directionsDisplay.setMap(map);
-            $scope.element = document.getElementById('map_canvas');
-            $scope.hammertime = Hammer($scope.element).on("hold", function(event) {
-                $scope.addOnClick(event);
-            });
 
-        },100);
+        directionsDisplay = new google.maps.DirectionsRenderer();
+        var latlng = start;
+        var myOptions = {
+            zoom: 15,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+        directionsDisplay.setMap(map);
+        $scope.element = document.getElementById('map_canvas');
+        $scope.hammertime = Hammer($scope.element).on("hold", function(event) {
+            $scope.addOnClick(event);
+        });
+        var oat = new google.maps.Marker({
+          position: new google.maps.LatLng(12.98899, 80.23361),
+          map: map,
+          title: 'Open Air Theatre'
+      }); 
+        var sac = new google.maps.Marker({
+          position: new google.maps.LatLng(12.98934, 80.23781),
+          map: map,
+          title: 'Stundents Activity Centre'
+      });   
+        var clt = new google.maps.Marker({
+            position: new google.maps.LatLng(12.98955 , 80.23189),
+            map: map,
+            title: 'Central Lecture Theatre'
+        }); 
+        google.maps.event.addListener(clt, 'click', function() {                ons.notification.alert({
+            message: 'Central Lecture Theatre'
+        });});
+        google.maps.event.addListener(oat, 'click', function() {                ons.notification.alert({
+            message: 'Open Air Theatre'
+        });});
+        google.maps.event.addListener(sac, 'click', function() {                ons.notification.alert({
+            message: 'Students Activity Centre'
+        });});
+
+    },100);
         //Directions api
         $scope.calcRoute=function () {
-        var end=document.getElementById('search').value;
-         var request = {
+            var end=document.getElementById('search').value;
+            var request = {
               origin:start,
               destination:end,
               travelMode: google.maps.TravelMode.WALKING
-           };
-         directionsService.route(request, function(response, status) {
+          };
+          directionsService.route(request, function(response, status) {
             if (status == google.maps.DirectionsStatus.OK) {
               directionsDisplay.setDirections(response);
-               }
-         });
-           } 
+          }
+      });
+      } 
 
 
         //Delete all Markers
@@ -154,15 +185,15 @@ function error(err) {
                         switch(idx) {
                             case 0:
 
-                                break;
+                            break;
                             case 1:
-                                for (var i = (partialDistance.length - 1); i >= 0 ; i--) {
+                            for (var i = (partialDistance.length - 1); i >= 0 ; i--) {
 
-                                    ons.notification.alert({
-                                        message: "The partial distance from point " + (i+1) + " to point " + (i+2) + " is " + partialDistance[i].toFixed(1) + " km"
-                                    });
-                                }
-                                break;
+                                ons.notification.alert({
+                                    message: "The partial distance from point " + (i+1) + " to point " + (i+2) + " is " + partialDistance[i].toFixed(1) + " km"
+                                });
+                            }
+                            break;
                         }
                     }
                 });
@@ -187,17 +218,17 @@ function error(err) {
 
             $timeout(function(){
                 //Creation of the listener associated to the Markers click
-            google.maps.event.addListener(marker, "click", function (e) {
-                ons.notification.confirm({
-                    message: 'Do you want to delete the marker?',
-                    callback: function(idx) {
-                        switch(idx) {
-                            case 0:
+                google.maps.event.addListener(marker, "click", function (e) {
+                    ons.notification.confirm({
+                        message: 'Do you want to delete the marker?',
+                        callback: function(idx) {
+                            switch(idx) {
+                                case 0:
                                 ons.notification.alert({
                                     message: 'You pressed "Cancel".'
                                 });
                                 break;
-                            case 1:
+                                case 1:
                                 for (var i = 0; i < $scope.markers.length; i++) {
                                     if ($scope.markers[i].id == marker.id) {
                                         //Remove the marker from Map                  
@@ -211,13 +242,13 @@ function error(err) {
                                     message: 'Marker deleted.'
                                 });
                                 break;
+                            }
                         }
-                    }
-                });
-            });
-            },1000);
+                    });
+});
+},1000);
 
 
-        };
-    });
+};
+});
 })();
